ee106b988611449cb7dedcb186e7333ccbb1234a
Tim Etimiri <timetimiri@gmail.com>
2019-11-02 06:07:56 +0000

    Add files via upload

---
+++ createLiveUSB.py
@@ -0,0 +1,17 @@
+import os
+import getpass 
+
+print("Creating a Live USB...")
+print ("WARNING: Make sure USB is formatted!")
+isoFile = input("Enter the path to the ISO file: ")
+
+os.system("diskutil list")
+USBnum = input("Enter the number at the end of the USB path: ")
+
+username = getpass.getuser()
+
+os.system("rm /Users/" + username + "/.youCanDeleteThis.dmg")
+os.system("hdiutil convert -format UDRW -o ~/.youCanDeleteThis " + isoFile)
+os.system("diskutil unmountDisk /dev/disk" + USBnum)
+print("WARNING: FLASHING USB. This may take a few minutes. Please be patient!")
+os.system("sudo dd if=/Users/" + username + "/.youCanDeleteThis.dmg of=/dev/rdisk" + USBnum + " bs=1m")
